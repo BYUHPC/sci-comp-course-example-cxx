1263f5584b6e6e0c38a3f0ddcc6a9d38ac7a5983
micgre93 <michaeljgreenburg@gmail.com>
2023-11-16 12:29:53 -0700

    Made initial.jl easier to understand

--- src/initial.jl
+++ src/initial.jl
@@ -21,11 +21,13 @@ end
 # Update dh to hold the result of `r-h³+∇²h`
 function dhdt!(dh, h, r, t)
     # Laplacian array; first and last cells are special cases due to boundary condition
-    L = [(h[begin+1]-h[begin])/2;
-         @views (h[begin:end-2].+h[begin+2:end])./2 .- h[begin+1:end-1];
-         (h[end-1]-h[end])/2]
+    L = [(h[begin+1] - h[begin])/2;
+         @views (h[begin:end-2] .+ h[begin+2:end]) ./2 .- h[begin+1:end-1];
+         (h[end  -1] - h[end  ])/2]
+
     # Update dh in place
     dh .= r .- h.^3 .+ L
+
     # No need to return anything since dh has been mutated
     return nothing
 end
@@ -37,11 +39,12 @@ function dsteepness(h, r)
     # Calculate growth rate
     g = similar(h)
     dhdt!(g, h, r, 0)
+
     # Calculate ds
     return sum(function(i)
         left  = max(i-1, firstindex(h))
         right = min(i+1,  lastindex(h))
-        return (h[right]-h[left]) * (g[right]-g[left]) / 2
+        return (h[right] - h[left]) * (g[right] - g[left]) / 2
     end, eachindex(h))/length(h)
 end
 
@@ -49,10 +52,16 @@ end
 
 # Solve and return a mountain range using ODEProblem
 function solve!(m::MountainRange)
-    cb = ContinuousCallback((h, t, integrator)->dsteepness(h, m.r), terminate!)
-    timebounds = (0.0, typemax(eltype(m.r)))
-    prob = ODEProblem{true}(dhdt!, m.h, timebounds, m.r, callback=cb)
+    # Callback that tells the solver when it's done (when dsteepness crosses zero)
+    terminationcondition(h, t, integrator) = dsteepness(h, m.r)
+    cb = ContinuousCallback(terminationcondition, terminate!)
+
+    # Define and solve the problem
+    timespan = (0.0, typemax(eltype(m.r)))
+    prob = ODEProblem{true}(dhdt!, m.h, timespan, m.r, callback=cb)
     sol = solve(prob)
+
+    # Update and return m
     m.h .= last(sol.u)
     m.t[] += last(sol.t)
     return m
@@ -62,11 +71,16 @@ end
 
 # Run the same problem as initial.cpp
 function main()
+    # Simulation parameters
     n = 1000
     plateau = 251:500
     r = zeros(n)
     r[plateau] .= 1
-    m = MountainRange(r, zeros(n), 0)
+    h = zeros(n)
+    t = 0.0
+
+    # Set up and solve the mountain range, printing the simulation time
+    m = MountainRange(r, h, t)
     solve!(m)
     println(m.t[])
 end
