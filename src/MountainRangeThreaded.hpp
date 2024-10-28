121eadd850c4a034b6f435fb2479b8983e07f2bc
Joshua Kelley <60236515+k-joshua-kelley@users.noreply.github.com>
2024-10-28 13:15:50 -0600

    Update MountainRangeThreaded.hpp
    Added comments explaining what everything is doing.

--- src/MountainRangeThreaded.hpp
+++ src/MountainRangeThreaded.hpp
@@ -31,20 +31,20 @@ namespace {
 
 
 
-class MountainRangeThreaded: public MountainRange {
+class MountainRangeThreaded: public MountainRange {  // Inherits from MountainRange, making all public things in MountainRange public in MountainRangeThreaded
     // Threading-related members
     bool continue_iteration; // used to tell the looping threadpool to terminate at the end of the simulation
     const size_type nthreads;
-    std::barrier<> ds_barrier, step_barrier;
-    std::vector<std::jthread> ds_workers, step_workers;
-    std::atomic<value_type> ds_aggregator; // used to reduce dsteepness from each thread
+    std::barrier ds_barrier, step_barrier; // creates two barrier objects.
+    std::vector<std::jthread> ds_workers, step_workers;  // creates two vectors of threads
+    std::atomic<value_type> ds_aggregator; // used to reduce dsteepness from each thread  // I'm not sure how this will be adapted to the wave function, I think dsteepness -> energy.
     value_type iter_dt; // Used to distribute dt to each thread
 
 
 
     // Determine which rows a certain thread is in charge of
     auto this_thread_cell_range(auto tid) {
-        return mr::split_range(cells, tid, nthreads);
+        return mr::split_range(cells, tid, nthreads);  // chops up the matrix and divies it up to the various threads.
     }
 
 
@@ -52,33 +52,35 @@ class MountainRangeThreaded: public MountainRange {
 public:
     // Help message to show that SOLVER_NUM_THREADS controls thread counts
     inline static const std::string help_message =
-            "Set the environment variable SOLVER_NUM_THREADS to a positive integer to set thread count (default 1).";
+            "Set the environment variable SOLVER_NUM_THREADS to a positive integer to set thread count (default 1).";  // static means create only one variable to be shared with all instances.
+// inline makes it so I don't have to go access this memory location each time, the complier just sticks it in wherever I call it (which is expensive unliess I only use it a couple times).
 
 
 
     // Run base constructor, then build threading infrastructure
-    MountainRangeThreaded(auto &&...args): MountainRange(args...), // https://tinyurl.com/byusc-parpack
+    // This constructor inherits the constructors from MountainRange and adds the following initializations.
+    MountainRangeThreaded(auto &&...args): MountainRange(args...), // https://tinyurl.com/byusc-parpack  // &&..args is a fancy way of saying what ever you pass to MountainRangeThreaded gets passed to MountainRange
             continue_iteration{true},
             nthreads{[]{ // https://tinyurl.com/byusc-lambdai
                 size_type nthreads = 1;
-                auto nthreads_str = std::getenv("SOLVER_NUM_THREADS");
-                if (nthreads_str != nullptr) std::from_chars(nthreads_str, nthreads_str+std::strlen(nthreads_str), nthreads);
+                auto nthreads_str = std::getenv("SOLVER_NUM_THREADS");  // Get the environment variable
+                if (nthreads_str != nullptr) std::from_chars(nthreads_str, nthreads_str+std::strlen(nthreads_str), nthreads);  // If the environment exists, set nthreads to the value fo the variable.
                 return nthreads;
-            }()},
-            ds_barrier(nthreads+1),   // worker threads plus main thread
+            }()},  // initialize the lambda function and then immediately call it.
+            ds_barrier(nthreads+1),   // worker threads plus main thread  // initialize the barrier with 8 worker threads plus the main/parent thread.
             step_barrier(nthreads+1), // worker threads plus main thread
             ds_workers(looping_threadpool(nthreads, [this](auto tid){ // https://tinyurl.com/byusc-lambda
-                ds_barrier.arrive_and_wait();
-                if (!continue_iteration) return false;
-                auto [first, last] = this_thread_cell_range(tid);
-                auto gfirst = tid==0 ? 1 : first;
-                auto glast  = tid==nthreads-1 ? last-1 : last;
-                value_type ds_local = 0;
-                for (size_t i=gfirst; i<glast; i++) ds_local += ds_cell(i);
-                ds_aggregator += ds_local;
-                ds_barrier.arrive_and_wait();
+                ds_barrier.arrive_and_wait();  // All the threads got to arrive before continuing
+                if (!continue_iteration) return false;  // terminating exit condition (see line 22)
+                auto [first, last] = this_thread_cell_range(tid);  // splits the matrix up and divies them out.
+                auto gfirst = tid==0 ? 1 : first;  // Gfirst = 1 tf tid==0 else Gfirst = first
+                auto glast  = tid==nthreads-1 ? last-1 : last;  // glast = last-1 if tid==nthreads-1 else glast = last
+                value_type ds_local = 0;  // aggregating/summation variable
+                for (size_t i=gfirst; i<glast; i++) ds_local += ds_cell(i); 
+                ds_aggregator += ds_local;  // atomic variable that adds all the results from all the threads.
+                ds_barrier.arrive_and_wait();  // wait for all threads to arrive before doing anything continuing.
                 return true;
-            })),
+            })),  // looping_threadpool takes in thread_count and a function.  The function is defined here as a lambda from lines 72-83
             step_workers(looping_threadpool(nthreads, [this](auto tid){ // https://tinyurl.com/byusc-lambda
                 step_barrier.arrive_and_wait();
                 if (!continue_iteration) return false;
@@ -99,7 +101,7 @@ public:
 
     // Destructor just tells threads to exit
     ~MountainRangeThreaded() {
-        continue_iteration = false;
+        continue_iteration = false;  // 
         ds_barrier.arrive_and_wait();   // signal ds_workers to exit
         step_barrier.arrive_and_wait(); // signal step_workers to exit
     }
@@ -143,3 +145,5 @@ public:
         return t;
     }
 };
+
+// A video of a walkthrough of the whole thing would be helpful.
